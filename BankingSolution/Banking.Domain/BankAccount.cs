53350d8b0bfa1d03a7d38a5955de8c375cb897c9
Mario Rodriguez <Mario_Rodriguez_36@progressive.com>
2023-02-09 11:19:30 -0500

    Added enumerated content

--- BankingSolution/Banking.Domain/BankAccount.cs
+++ BankingSolution/Banking.Domain/BankAccount.cs
@@ -1,13 +1,21 @@
 ﻿namespace Banking.Domain;
 
+public enum LoyaltyLevel { Standard, Gold };
+
 public class BankAccount
 {
     decimal _balance = 5000m;
+    public LoyaltyLevel Level;
 
     public void Deposit(decimal amountToDeposit)
     {
-        _balance += amountToDeposit;
-        return;
+        decimal bonus = 0m;
+        
+        if (Level== LoyaltyLevel.Gold) { 
+            bonus = amountToDeposit * .10m;
+        }
+
+        _balance += amountToDeposit + bonus;
     }
 
     public decimal GetBalance()
