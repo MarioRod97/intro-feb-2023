881ad5e94f7da0295a31ca1c12ddd24d0629653e
Mario Rodriguez <Mario_Rodriguez_36@progressive.com>
2023-02-09 11:23:02 -0500

    New test

---
+++ BankingSolution/Banking.UnitTest/GoldCustomersGetBonusOnDeposits.cs
@@ -0,0 +1,17 @@
+﻿namespace Banking.UnitTest;
+
+public class GoldCustomersGetBonusOnDeposits
+{
+    [Fact]
+    public void BonusAppliedToDeposit()
+    {
+        var account = new BankAccount();
+        account.Level = LoyaltyLevel.Gold;
+        var openingBalance = account.GetBalance();
+        var amountToDeposit = 100m;
+
+        account.Deposit(amountToDeposit);
+
+        Assert.Equal(openingBalance + amountToDeposit + 10m, account.GetBalance());
+    }
+}
